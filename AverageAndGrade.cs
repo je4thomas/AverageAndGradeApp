c6b384d22d6215089b4752afb1c3b01a4852b01f
je4thomas <eric.thomas.az@gmail.com>
2012-06-27 21:50:45 -0700

    intial project version with MikeN

---
+++ AverageAndGrade.cs
@@ -0,0 +1,103 @@
+﻿/*AverageAndGradeApp.cs
+ * Author: Joseph Thomas
+ * This application will
+ * take a student's 5 scores.
+ * It will construct an obect of
+ * the Grades class will
+ * calculate the average. 
+ * It will then call DetermineGrade
+ * to determine the appropriate
+ * grade from A to F.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AverageAndGradeApp
+{
+    class AverageAndGrade
+    {
+        static void Main(string[] args)
+        {
+            int scoreResults;
+            double scrRes;
+            char grade = 'i';
+            
+            DisplayInstructions();
+
+            Grades myGradeResults = new Grades();
+            myGradeResults.Score1 = GetScores("number 1 ");
+            myGradeResults.Score2 = GetScores("number 2 ");
+            myGradeResults.Score3 = GetScores("number 3 ");
+            myGradeResults.Score4 = GetScores("number 4 ");
+            myGradeResults.Score5 = GetScores("number 5 ");
+
+            // Gets the Score Average from Grades.cs
+            scrRes = myGradeResults.ScoreAve();
+            // Converts the double average to an int for grade determination
+            scoreResults = Convert.ToInt32(scrRes);
+
+            grade = DetermineGrade(scoreResults);
+            
+            DisplayResults(scoreResults, grade);
+
+        }
+
+        public static char DetermineGrade(double scr)
+        {
+            char grd = 'Q';
+
+            if (scr < 60.0)
+                grd = 'F';
+            else if (scr < 70.0)
+                grd = 'D';
+            else if (scr < 80.0)
+                grd = 'C';
+            else if (scr < 90.0)
+                grd = 'B';
+            else if (scr <= 100.0)
+                grd = 'A';
+
+            return grd;
+        }
+
+
+        public static void DisplayResults(double scravg, char grd)
+        {
+            Console.Clear();
+            Console.WriteLine("AVERAGE & GRADE RESULTS");
+            Console.Write("\n\nAverage = \t{0:F0}", scravg);
+            Console.WriteLine("\t " + grd);
+            Console.WriteLine("\n\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+  
+
+        // Request Scores from User      
+        public static int GetScores(string scoreNumber)
+        {      
+            int usersScore;
+            string inputValue;
+
+            Console.Write("Enter the score for exam {0} : ", scoreNumber);
+            inputValue = Console.ReadLine();
+            usersScore = int.Parse(inputValue);
+            return usersScore;
+        }
+
+
+        public static void DisplayInstructions()
+        {
+            // Display the Instructions for User to Follow
+            Console.WriteLine("{0, 45}", "Calculate Average & Determine Grade");
+            Console.WriteLine("\n\nThis program averages the 5 scores of the user.");
+            Console.WriteLine("The user will input the scores of their 5 exams");
+            Console.WriteLine("The program will then calculate the Student's average \nand determine the appropriate grade.");
+            Console.WriteLine("\n\nPress any key to begin...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
---
+++ Grades.cs
@@ -0,0 +1,153 @@
+﻿/*Grade.cs
+ * Author: Joseph Thomas
+ * Date: 06/24/2012
+ * This class defines a template
+ * for a student's scores that 
+ * includes 5 scores and a method
+ * that calculates the score average.
+ * It also overrides ToString()
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AverageAndGradeApp
+{
+    class Grades
+    {
+        private int score1;
+        private int score2;
+        private int score3;
+        private int score4;
+        private int score5;
+        private double scoreAvg;
+
+        // Property acting as mutator and accessor
+        public int Score1
+        {
+            get 
+            {
+                return score1;
+            }
+            set
+            {
+                score1 = value;
+            }
+        }
+
+        // Property acting as mutator and accessor
+        public int Score2
+        {
+            get
+            {
+                return score2;
+            }
+            set 
+            {
+                score2 = value;
+            }
+        }
+
+        // Property acting as mutator and accessor
+        public int Score3
+        {
+            get
+            {
+                return score3;
+            }
+            set
+            {
+                score3= value;
+            }
+        }
+
+        // Property acting as mutator and accessor
+        public int Score4
+        {
+            set
+            {
+                score4 = value;
+            }
+
+            get
+            {
+                return score4;
+            }
+
+        }
+
+        // Property acting as mutator and accessor
+        public int Score5
+        {
+            get
+            {
+                return score5;
+            }
+            set
+            {
+                score5 = value;
+            }
+        }
+
+        // Default Constructor
+        public Grades()
+        {
+        }
+
+        // One Parameter Constructor
+        public Grades(int scr1)
+        {
+            score1 = scr1;
+        }
+
+        // Two Score Constructor
+        public Grades(int scr1, int scr2)
+        {
+            score1 = scr1;
+            score2 = scr2;
+        }
+
+        // Three Score Constructor
+        public Grades(int scr1, int scr2, int scr3)
+        {
+            score1 = scr1;
+            score2 = scr2;
+            score3 = scr3;
+        }
+
+        // Four Score Constructor
+        public Grades(int scr1, int scr2, int scr3, int scr4)
+        {
+            score1 = scr1;
+            score2 = scr2;
+            score3 = scr3;
+            score4 = scr4;
+        }
+
+        // Five Score Constructor
+        public Grades(int scr1, int scr2, int scr3, int scr4, int scr5)
+        {
+            score1 = scr1;
+            score2 = scr2;
+            score3 = scr3;
+            score4 = scr4;
+            score5 = scr5;
+        }
+
+        // Calculates the Average of the 5 Scores Entered
+        public double ScoreAve()
+        {
+            scoreAvg = ((score1 + score2 + score3 + score4 + score5) / 5.0);
+            return scoreAvg;
+        }
+
+        // Override the ToString for Grades.cs
+        public override string ToString()
+        {
+            return "Student's Scores" + "\n\tScore1 = " + score1 + "\n\tScore2 = " + score2 + "\n\tScore3 = " + score3 + "\n\tScore4 = " + score4 + "\n\tScore5 = " + score5 + "\n\t_________________________" + "\n\tAverage = " + ScoreAve().ToString("F0");
+        }
+
+    }
+}
